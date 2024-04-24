9b54d4b625198ef677d515f895b1ce1ba53b85f7
vamman <vamman@gmail.com>
2024-04-24 02:07:02 -0300

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -6,3 +6,15 @@ wget https://www.pixelos.com/downloads/kernel/6.4.11/debian/debs/linux-image-6.4
 wget https://www.pixelos.com/downloads/kernel/6.4.11/debian/debs/linux-image-6.4.11-tkg-pixelos_6.4.11-1_amd64.deb
 wget https://www.pixelos.com/downloads/kernel/6.4.11/debian/debs/linux-libc-dev_6.4.11-1_amd64.deb
 dpkg -i *
+
+# Install kdump
+apt-get update
+apt-get install -y kdump-tools
+
+# Configure kdump
+cat <<EOF >> /etc/default/kdump-tools
+KDUMP_COMMANDLINE_APPEND="reboot"
+EOF
+
+# Enable kdump
+systemctl enable kdump-tools
