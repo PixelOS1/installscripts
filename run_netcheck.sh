67301ba34d8836d13d76127747490a4b48fc570f
vamman <vamman@gmail.com>
2024-04-23 22:38:50 -0300

    Create run_netcheck.sh

---
+++ run_netcheck.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+flagFile="ip_check_flag.txt"
+
+while true; do
+    # Check if the flag file exists
+    if [ ! -e "$flagFile" ]; then
+        # Fetch the public IP address of the server using checkip.amazonaws.com
+        ipAddress=$(curl -s https://checkip.amazonaws.com/)
+
+        response=$(curl -s "https://www.pixelos.com/scripts/checkip.php?ip_to_check=$ipAddress")
+
+        name=$(echo "$response" | jq -r '.name')
+
+        if [ "$name" != "null" ]; then
+            sudo hostnamectl set-hostname "$name"
+
+            touch "$flagFile"
+            echo "Flag file created."
+            /bin/bash /root/netdata.sh
+        fi
+    else
+        break
+    fi
+done
