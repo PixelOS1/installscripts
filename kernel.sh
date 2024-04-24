5fd19fa28ebc3c6138511fe9d764d224ed36f632
vamman <vamman@gmail.com>
2024-04-24 01:38:56 -0300

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -6,4 +6,3 @@ wget https://www.pixelos.com/downloads/kernel/6.4.11/debian/debs/linux-image-6.4
 wget https://www.pixelos.com/downloads/kernel/6.4.11/debian/debs/linux-image-6.4.11-tkg-pixelos_6.4.11-1_amd64.deb
 wget https://www.pixelos.com/downloads/kernel/6.4.11/debian/debs/linux-libc-dev_6.4.11-1_amd64.deb
 dpkg -i *
-reboot
