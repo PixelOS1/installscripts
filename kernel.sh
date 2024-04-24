f06b7b71f2ce8583923e5c5d674cf3622682f8d2
vamman <vamman@gmail.com>
2024-04-24 02:47:32 -0300

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -6,3 +6,7 @@ wget https://www.pixelos.com/downloads/kernel/6.4.11/debian/debs/linux-image-6.4
 wget https://www.pixelos.com/downloads/kernel/6.4.11/debian/debs/linux-image-6.4.11-tkg-pixelos_6.4.11-1_amd64.deb
 wget https://www.pixelos.com/downloads/kernel/6.4.11/debian/debs/linux-libc-dev_6.4.11-1_amd64.deb
 dpkg -i *
+
+# Kernel was compiled with gcc v12
+sudo apt install --reinstall gcc-12
+sudo ln -s -f /usr/bin/gcc-12 /usr/bin/gcc
