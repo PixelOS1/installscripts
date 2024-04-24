eb57f4fb9e3fa5729a58532a96376d4b91b6858e
vamman <vamman@gmail.com>
2024-04-23 23:27:02 -0300

    Update user.sh

--- user.sh
+++ user.sh
@@ -2,3 +2,9 @@
 useradd user
 usermod -aG sudo user
 ufw disable
+
+# Get the UID of the "user" account
+USERID=$(id -u user)
+
+# Set environment variables
+echo "USERID=$USERID" >> /etc/environment
