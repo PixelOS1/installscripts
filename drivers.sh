be4cddce469db68122637abad06aaa2d89b61467
vamman <vamman@gmail.com>
2024-04-25 00:11:00 -0300

    Update drivers.sh

--- drivers.sh
+++ drivers.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-sudo rmmod nvidia_drm nvidia_modeset nvidia_uvm nvidia
 mkdir -p /opt/nvidia/drivers/
 wget http://169.254.169.254/latest/nvidia_linux_driver_url -O /opt/nvidia/drivers/nvidia_manual_driver.run
 chmod +x /opt/nvidia/drivers/nvidia_manual_driver.run
 /opt/nvidia/drivers/nvidia_manual_driver.run --ui=none --no-questions
-nvidia-smi
+reboot
