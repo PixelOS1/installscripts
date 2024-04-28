2faf5ef96ec8bd1558d7f56c2db44f0a22c5b223
vamman <vamman@gmail.com>
2024-04-28 05:24:18 -0300

    Update run_netcheck.sh

--- run_netcheck.sh
+++ run_netcheck.sh
@@ -3,23 +3,26 @@
 flagFile="/opt/pixelos/installscripts/ip_check_flag.txt"
 
 while true; do
-    # Check if the flag file exists
-    if [ ! -e "$flagFile" ]; then
-        # Fetch the public IP address of the server using checkip.amazonaws.com
-        ipAddress=$(curl -s https://checkip.amazonaws.com/)
-
-        response=$(curl -s "https://www.pixelos.com/scripts/checkip.php?ip_to_check=$ipAddress")
-
-        name=$(echo "$response" | jq -r '.name')
-
-        if [ "$name" != "null" ]; then
-            sudo hostnamectl set-hostname "$name"
-
-            touch "$flagFile"
-            echo "Flag file created."
-            /bin/bash /opt/pixelos/installscripts/netdata.sh
-        fi
-    else
+    # Check if the flag file exists and is not empty
+    if [ -s "$flagFile" ]; then
+        /bin/bash /opt/pixelos/installscripts/netdata.sh
         break
+    fi
+
+    # Fetch the public IP address of the server using checkip.amazonaws.com
+    ipAddress=$(curl -s https://checkip.amazonaws.com/)
+
+    response=$(curl -s "https://www.pixelos.com/scripts/checkip.php?ip_to_check=$ipAddress")
+
+    name=$(echo "$response" | jq -r '.name')
+
+    if [ "$name" != "null" ]; then
+        sudo hostnamectl set-hostname "$name"
+
+        touch "$flagFile"
+        echo "Flag file created."
+
+        # Write a message to a file
+        echo "Server hostname updated to $name" > "$flagFile"
     fi
 done
